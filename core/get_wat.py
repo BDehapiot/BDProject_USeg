ff1cfd3743ac1321289e6fa5b3223432574635fd
bdehapiot <b.dehapiot@gmail.com>
2022-10-16 09:46:13 +0200

    update

--- core/get_wat.py
+++ core/get_wat.py
@@ -11,8 +11,8 @@ from skimage.filters import threshold_li
 from skimage.filters import sato, gaussian
 from skimage.restoration import rolling_ball
 from skimage.measure import label, regionprops
-from skimage.segmentation import watershed, clear_border, expand_labels
-from skimage.morphology import remove_small_objects, binary_dilation, remove_small_holes
+from skimage.segmentation import watershed, clear_border, expand_labels, find_boundaries
+from skimage.morphology import remove_small_objects, binary_dilation, remove_small_holes, skeletonize
 
 #%% Function (get_wat) --------------------------------------------------------
 
@@ -57,17 +57,17 @@ def get_wat(
         max_size = max([i.area for i in props])
         mask = remove_small_objects(mask, min_size=max_size*0.01)
         
-        # Get watershed 
-        markers = label(np.invert(mask), connectivity=1)  
-        labels = watershed(
-            ridges, markers, compactness=1, watershed_line=True
-            ) 
+        # # Get watershed 
+        # markers = label(np.invert(mask), connectivity=1)  
+        # labels = watershed(
+        #     ridges, markers, compactness=1, watershed_line=False
+        #     ) 
                 
-        # Remove border cells
-        if remove_border_cells:
-            labels = clear_border(labels)
+        # # Remove border cells
+        # if remove_border_cells:
+        #     labels = clear_border(labels)
             
-        wat = labels == 0
+        # wat = labels == 0
         
         # # Remove small cells
         # if min_cell_size > 0:
@@ -88,7 +88,7 @@ def get_wat(
         # temp_mask = binary_dilation(labels > 0)
         # wat[temp_mask == 0] = 0
 
-        return rsize, ridges, mask, labels, wat
+        return rsize, ridges, mask, # labels, wat
     
     # Run ---------------------------------------------------------------------
     
@@ -99,7 +99,8 @@ def get_wat(
         
     # Adjust parameters according to binning
     ridge_size = ridge_size/binning
-        
+    min_cell_size = min_cell_size/binning
+    
     if parallel:
     
         # Run parallel
@@ -127,10 +128,10 @@ def get_wat(
             [img[1] for img in output_list], axis=0).squeeze(),
         'mask': np.stack(
             [img[2] for img in output_list], axis=0).squeeze(),
-        'labels': np.stack(
-            [img[3] for img in output_list], axis=0).squeeze(),
-        'wat':np.stack(
-            [img[4] for img in output_list], axis=0).squeeze(),
+        # 'labels': np.stack(
+        #     [img[3] for img in output_list], axis=0).squeeze(),
+        # 'wat':np.stack(
+        #     [img[4] for img in output_list], axis=0).squeeze(),
         }
     
     return output_dict
@@ -143,7 +144,7 @@ def get_wat(
 raw_name = 'Disc_Fixed_118hAEL_disc04_uint8_crop.tif'
 
 # Parameters
-binning = 1
+binning = 2
 ridge_size = 3
 thresh_coeff = 0.5
 min_cell_size = 0
@@ -183,33 +184,56 @@ print(f'  {(end-start):5.3f} s')
 rsize = output_dict['rsize']
 ridges = output_dict['ridges']
 mask = output_dict['mask']
-labels = output_dict['labels']
-wat = output_dict['wat']
+# labels = output_dict['labels']
+# wat = output_dict['wat']
+
+# Parameters
+min_cell_size = 50/binning
+remove_border_cells = True
+
+start = time.time()
+print('test')
+
+# Option #1 -------------------------------------------------------------------
+
+# Get watershed 
+markers = label(np.invert(mask), connectivity=1)  
+labels = watershed(
+    ridges, markers, compactness=5, watershed_line=False
+    ) 
 
 # Remove small cells
-min_cell_size = 50
 if min_cell_size > 0:
     
-    temp_mask = labels == 0
-    temp_mask = remove_small_holes(
-        temp_mask, area_threshold=min_cell_size
-        )
-    
-#     temp_mask = remove_small_holes(
-#         temp_mask, area_threshold=min_cell_size
-#         )
-#     labels[temp_mask==1] = 0
-#     labels = expand_labels(labels, distance=min_cell_size)
-    
-#     # temp_mask = binary_dilation(labels > 0)
-#     # temp_mask = remove_small_holes(
-#     #     temp_mask, 
-#     #     area_threshold=min_cell_size
-#     #     )
-#     # labels = expand_labels(labels, distance=min_cell_size)
-#     # labels[temp_mask == 0] = 0  
+    props = regionprops(labels)
+    for i, prop in enumerate(props):
+        if prop.area < min_cell_size:
+            labels[labels==i+1] = 0
+            
+    labels = expand_labels(labels, distance=min_cell_size)
 
+# Remove border cells
+if remove_border_cells:
+    labels = clear_border(labels)    
+    
+# Get boundaries
+wat = find_boundaries(labels)
+wat = skeletonize(wat, method='zhang')
+labels = label(np.invert(wat), connectivity=1)
+
+# Option #2 -------------------------------------------------------------------
+
+# # Get watershed 
+# markers = label(np.invert(mask), connectivity=1)  
+# labels = watershed(
+#     ridges, markers, compactness=1, watershed_line=True
+#     ) 
+
+end = time.time()
+print(f'  {(end-start):5.3f} s')
+
+# Display
 viewer = napari.Viewer()
-viewer.add_image(temp_mask)
+viewer.add_image(wat)
 viewer.add_labels(labels)
-viewer.grid.enabled = True
+# viewer.grid.enabled = True
