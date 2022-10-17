87478235d7a0d6bf053f255cb12e559c62ed1a64
bdehapiot <b.dehapiot@gmail.com>
2022-10-17 10:21:46 +0200

    update

--- core/get_wat.py
+++ core/get_wat.py
@@ -56,39 +56,25 @@ def get_wat(
         props = regionprops(label(mask))
         max_size = max([i.area for i in props])
         mask = remove_small_objects(mask, min_size=max_size*0.01)
-        
-        # # Get watershed 
-        # markers = label(np.invert(mask), connectivity=1)  
-        # labels = watershed(
-        #     ridges, markers, compactness=1, watershed_line=False
-        #     ) 
                 
-        # # Remove border cells
-        # if remove_border_cells:
-        #     labels = clear_border(labels)
+        # Get markers & labels
+        markers = label(np.invert(mask), connectivity=1)  
+        markers = remove_small_objects(markers, min_size=min_cell_size)
+        labels = watershed(
+            ridges, markers, compactness=10/binning, watershed_line=True
+            ) 
+        
+        # Remove border cells
+        if remove_border_cells:
+            labels = clear_border(labels)
             
-        # wat = labels == 0
-        
-        # # Remove small cells
-        # if min_cell_size > 0:
-        #     temp_mask = binary_dilation(labels > 0)
-        #     temp_mask = remove_small_holes(
-        #         temp_mask, 
-        #         area_threshold=min_cell_size
-        #         )
-        #     labels = expand_labels(labels, distance=min_cell_size)
-        #     labels[temp_mask == 0] = 0  
-        
-        # # # Remove isolated cells
-        # temp_mask = labels > 0
-        # temp_mask = remove_small_objects(temp_mask, min_size = max_size*0.01)
-        # labels[temp_mask==0] = 0  
-        
-        # # Clean wat & vertices
-        # temp_mask = binary_dilation(labels > 0)
-        # wat[temp_mask == 0] = 0
-
-        return rsize, ridges, mask, # labels, wat
+        # Get wat & updated labels
+        wat = labels == 0
+        temp_wat = binary_dilation(wat == 0)
+        wat = np.invert(wat^temp_wat)
+        labels = label(np.invert(wat), connectivity=1)
+                
+        return rsize, ridges, mask, markers, labels, wat
     
     # Run ---------------------------------------------------------------------
     
@@ -128,10 +114,12 @@ def get_wat(
             [img[1] for img in output_list], axis=0).squeeze(),
         'mask': np.stack(
             [img[2] for img in output_list], axis=0).squeeze(),
-        # 'labels': np.stack(
-        #     [img[3] for img in output_list], axis=0).squeeze(),
-        # 'wat':np.stack(
-        #     [img[4] for img in output_list], axis=0).squeeze(),
+        'markers': np.stack(
+            [img[3] for img in output_list], axis=0).squeeze(),
+        'labels': np.stack(
+            [img[4] for img in output_list], axis=0).squeeze(),
+        'wat':np.stack(
+            [img[5] for img in output_list], axis=0).squeeze(),
         }
     
     return output_dict
@@ -139,15 +127,16 @@ def get_wat(
 #%% Run -----------------------------------------------------------------------
 
 # File name
-# raw_name = '13-12-06_40x_GBE_eCad_Ctrl_#19_uint8.tif'
+raw_name = '13-12-06_40x_GBE_eCad_Ctrl_#19_uint8.tif'
 # raw_name = '18-03-12_100x_GBE_UtrCH_Ctrl_b3_uint8.tif'
-raw_name = 'Disc_Fixed_118hAEL_disc04_uint8_crop.tif'
+# raw_name = 'Disc_Fixed_118hAEL_disc04_uint8_crop.tif'
 
 # Parameters
 binning = 2
 ridge_size = 3
 thresh_coeff = 0.5
-min_cell_size = 0
+min_cell_size = 25
+remove_border_cells = True
 
 # Open data
 raw = io.imread(Path('../data/', raw_name))
@@ -162,7 +151,7 @@ output_dict = get_wat(
     ridge_size, 
     thresh_coeff, 
     min_cell_size,
-    remove_border_cells=False, 
+    remove_border_cells=True, 
     parallel=True
     )
 
@@ -171,63 +160,44 @@ print(f'  {(end-start):5.3f} s')
 
 #%% Display -------------------------------------------------------------------
 
-# viewer = napari.Viewer()
-# viewer.add_image(output_dict['wat'])
-# viewer.add_labels(output_dict['labels'])
-# viewer.add_image(output_dict['mask'])
-# viewer.add_image(output_dict['ridges'])
-# viewer.add_image(output_dict['rsize'])
-# viewer.grid.enabled = True
+viewer = napari.Viewer()
+viewer.add_image(output_dict['wat'])
+viewer.add_labels(output_dict['labels'])
+viewer.add_labels(output_dict['markers'])
+viewer.add_image(output_dict['mask'])
+viewer.add_image(output_dict['ridges'])
+viewer.add_image(output_dict['rsize'])
+viewer.grid.enabled = True
 
 #%% Tests ---------------------------------------------------------------------
 
-rsize = output_dict['rsize']
-ridges = output_dict['ridges']
-mask = output_dict['mask']
+# rsize = output_dict['rsize']
+# ridges = output_dict['ridges']
+# mask = output_dict['mask']
+# markers = output_dict['markers']
 # labels = output_dict['labels']
 # wat = output_dict['wat']
 
-# Parameters
-min_cell_size = 50/binning
-remove_border_cells = True
-
-start = time.time()
-print('test')
+# start = time.time()
+# print('test')
 
 # Option #1 -------------------------------------------------------------------
 
-# Get watershed 
-markers = label(np.invert(mask), connectivity=1)  
-labels = watershed(
-    ridges, markers, compactness=10/binning, watershed_line=False
-    ) 
-
-# # Remove small cells
-labels = remove_small_objects(labels, min_size=min_cell_size)
-labels = expand_labels(labels, distance=min_cell_size)
-
-# Remove border cells
-if remove_border_cells:
-    labels = clear_border(labels)    
-    
-# Get boundaries
-wat = find_boundaries(labels)
-# wat = skeletonize(wat, method='lee')
-# labels = label(np.invert(wat), connectivity=1)
+# wat = labels == 0
+# temp_wat = wat == 0
+# temp_wat = binary_dilation(temp_wat)
+# wat = np.invert(wat^temp_wat)
+# wat = label(np.invert(wat), connectivity=1)
 
 # Option #2 -------------------------------------------------------------------
 
-# # Get watershed 
-# markers = label(np.invert(mask), connectivity=1)  
-# labels = watershed(
-#     ridges, markers, compactness=1, watershed_line=True
-#     ) 
-
-end = time.time()
-print(f'  {(end-start):5.3f} s')
+# end = time.time()
+# print(f'  {(end-start):5.3f} s')
 
 # Display
-viewer = napari.Viewer()
-viewer.add_image(wat)
-viewer.add_labels(labels)
+# viewer = napari.Viewer()
+# viewer.add_labels(markers)
+# viewer.add_labels(labels)
+# viewer.add_labels(wat)
+# viewer.add_image(temp_wat)
 # viewer.grid.enabled = True
