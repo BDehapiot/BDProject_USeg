b8ad98ab72562948bd5bf98222e349480e9903cf
bdehapiot <b.dehapiot@gmail.com>
2022-10-18 17:16:25 +0200

    update

--- core/get_wat.py
+++ core/get_wat.py
@@ -9,16 +9,18 @@ from joblib import Parallel, delayed
 from skimage.transform import resize
 from skimage.filters import threshold_li
 from skimage.filters import sato, gaussian
+from scipy.ndimage import binary_fill_holes
 from skimage.restoration import rolling_ball
 from skimage.measure import label, regionprops
-from skimage.segmentation import watershed, clear_border
-from skimage.morphology import remove_small_objects, binary_dilation
+from skimage.segmentation import watershed, clear_border, expand_labels
+from skimage.morphology import remove_small_objects, binary_dilation, remove_small_holes
 
 #%% To do list ----------------------------------------------------------------
 
 '''
 - Extract advanced parameters
-- Filter small and big cells using the same parameter
+- Auto ridge_size
+
 '''
 
 #%% Function (get_wat) --------------------------------------------------------
@@ -64,24 +66,35 @@ def get_wat(
         max_size = max([i.area for i in props])
         mask = remove_small_objects(mask, min_size=max_size*0.01)
                 
-        # Get markers & labels
+        # Get markers
         markers = label(np.invert(mask), connectivity=1)  
-        markers = remove_small_objects(markers, min_size=min_cell_size)
-        labels = watershed(
-            ridges, markers, compactness=10/binning, watershed_line=True
-            ) 
         
+        # Remove small and big objects
+        props = regionprops(markers)
+        areas = np.array([i.area for i in props])
+        idx = np.array([i.label for i in props])
+        med = np.median(areas)
+        for i, area in enumerate(areas):
+            if area < med/5: # Remove small objects
+                markers[markers==idx[i]] = 0        
+            # if area > med*10: # Remove big objects
+            #     markers[markers==idx[i]] = 0
+        
+        # Get labels (with watershed)
+        labels = watershed(
+            ridges, 
+            markers, 
+            compactness=10/binning, 
+            watershed_line=True
+            ) 
+                
         # Remove border cells
         if remove_border_cells:
             labels = clear_border(labels)
              
         # Get wat & update labels
         wat = labels == 0
-        temp = binary_dilation(wat == 0) 
-        temp = remove_small_objects(temp, min_size=max_size*0.01) 
-        wat = wat*temp # Remove isolated cells
-        labels = label(np.invert(wat), connectivity=1)
-                        
+        
         return rsize, ridges, mask, markers, labels, wat
     
     # Run ---------------------------------------------------------------------
@@ -135,72 +148,178 @@ def get_wat(
 #%% Run -----------------------------------------------------------------------
 
 # File name
-raw_name = '13-12-06_40x_GBE_eCad_Ctrl_#19_uint8.tif'
+# raw_name = '13-12-06_40x_GBE_eCad_Ctrl_#19_uint8.tif'
 # raw_name = '13-03-06_40x_GBE_eCad(Carb)_Ctrl_#98_uint8.tif'
 # raw_name = '18-03-12_100x_GBE_UtrCH_Ctrl_b3_uint8.tif'
 # raw_name = 'Disc_Fixed_118hAEL_disc04_uint8_crop.tif'
-# raw_name = 'Disc_ex_vivo_118hAEL_disc2_uint8.tif'
+raw_name = 'Disc_ex_vivo_118hAEL_disc2_uint8.tif'
 
 # Parameters
 binning = 2
-ridge_size = 2
+ridge_size = 1.5
 thresh_coeff = 0.5
-min_cell_size = 25
-remove_border_cells = False
+min_cell_size = 100
+remove_border_cells = True
 
 # Open data
 raw = io.imread(Path('../data/', raw_name))
 
-# Process data (get_wat)
-start = time.time()
-print('get_wat')
+# # Process data (get_wat)
+# start = time.time()
+# print('get_wat')
 
-output_dict = get_wat(
-    raw, 
-    binning, 
-    ridge_size, 
-    thresh_coeff, 
-    min_cell_size,
-    remove_border_cells=remove_border_cells, 
-    parallel=True
+# output_dict = get_wat(
+#     raw, 
+#     binning, 
+#     ridge_size, 
+#     thresh_coeff, 
+#     min_cell_size,
+#     remove_border_cells=remove_border_cells, 
+#     parallel=True
+#     )
+
+# end = time.time()
+# print(f'  {(end-start):5.3f} s')
+
+#%% Display -------------------------------------------------------------------
+
+# viewer = napari.Viewer()
+# viewer.add_image(output_dict['wat'])
+# viewer.add_labels(output_dict['labels'])
+# viewer.add_labels(output_dict['markers'])
+# viewer.add_image(output_dict['mask'])
+# viewer.add_image(output_dict['ridges'])
+# viewer.add_image(output_dict['rsize'])
+# viewer.grid.enabled = True
+
+#%% Tests ---------------------------------------------------------------------
+
+# rsize = output_dict['rsize']
+# ridges = output_dict['ridges']
+# mask = output_dict['mask']
+# markers = output_dict['markers']
+# labels = output_dict['labels']
+# wat = output_dict['wat']
+
+# t = -1
+# rsize = output_dict['rsize'][t,...]
+# ridges = output_dict['ridges'][t,...]
+# mask = output_dict['mask'][t,...]
+# markers = output_dict['markers'][t,...]
+# labels = output_dict['labels'][t,...]
+# wat = output_dict['wat'][t,...]
+
+# raw = raw[-1,...]
+
+# -----------------------------------------------------------------------------
+
+start = time.time()
+print('Resize (according to binning) ')
+
+# Resize (according to binning)  
+rsize = resize(raw, (
+    int(raw.shape[0]//binning), 
+    int(raw.shape[1]//binning)),
+    preserve_range=True, 
+    anti_aliasing=True,
+    ) 
+
+end = time.time()
+print(f'  {(end-start):5.3f} s')
+
+# -----------------------------------------------------------------------------
+
+start = time.time()
+print('Subtract background')
+
+# Subtract background
+rsize -= rolling_ball(
+    gaussian(rsize, sigma=ridge_size//2), radius=ridge_size*2,
+    ) 
+
+end = time.time()
+print(f'  {(end-start):5.3f} s')
+
+# -----------------------------------------------------------------------------
+
+start = time.time()
+print('Apply ridge filter ')
+
+# Apply ridge filter 
+ridges = sato(
+    rsize, sigmas=ridge_size, mode='reflect', black_ridges=False,
     )
 
 end = time.time()
 print(f'  {(end-start):5.3f} s')
 
-#%% Display -------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
-viewer = napari.Viewer()
-viewer.add_image(output_dict['wat'])
-viewer.add_labels(output_dict['labels'])
-viewer.add_labels(output_dict['markers'])
-viewer.add_image(output_dict['mask'])
-viewer.add_image(output_dict['ridges'])
-viewer.add_image(output_dict['rsize'])
-viewer.grid.enabled = True
+start = time.time()
+print('Get mask & markers')
 
-#%% Tests ---------------------------------------------------------------------
+# Get mask
+thresh = threshold_li(ridges, tolerance=1)
+mask = ridges > thresh*thresh_coeff
+mask = remove_small_objects(mask, min_size=np.sum(mask)*0.01)
+markers = label(np.invert(mask), connectivity=1)  
 
-# rsize = output_dict['rsize'][-1,...]
-# ridges = output_dict['ridges'][-1,...]
-# mask = output_dict['mask'][-1,...]
-# markers = output_dict['markers'][-1,...]
-# labels = output_dict['labels'][-1,...]
-# wat = output_dict['wat'][-1,...]
+end = time.time()
+print(f'  {(end-start):5.3f} s')
 
-# start = time.time()
-# print('test')
+# -----------------------------------------------------------------------------
 
-# Option #1 -------------------------------------------------------------------
+start = time.time()
+print('Filter objects (according to labels area)')
 
-# Option #2 -------------------------------------------------------------------
+# Filter objects (according to labels area)
+props = regionprops(markers)
+idxs = np.array([i.label for i in props])
+areas = np.array([i.area for i in props])
+median = np.median(areas)
+for idx, area in zip(idxs, areas):
+    if area < median/5: # Remove small objects
+        markers[markers==idx] = 0 
+    if area > median*10: # Remove large objects
+        markers[markers==idx] = np.max(markers) + 1 
+        
+end = time.time()
+print(f'  {(end-start):5.3f} s') 
 
-# end = time.time()
-# print(f'  {(end-start):5.3f} s')
+# -----------------------------------------------------------------------------
+
+start = time.time()
+print('Get watershed labels & line')
+
+# Get watershed labels & line
+labels = watershed(
+    ridges, 
+    markers, 
+    compactness=10/binning, 
+    watershed_line=True
+    ) 
+
+if remove_border_cells:
+    labels = clear_border(labels, bgval=np.max(labels)+1)
+
+line = labels == 0
+             
+end = time.time()
+print(f'  {(end-start):5.3f} s') 
+
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
 
 # Display
-# viewer = napari.Viewer()
-# viewer.add_labels(markers)
-# viewer.add_labels(labels)
-# viewer.add_image(wat)
-# viewer.grid.enabled = True
+viewer = napari.Viewer()
+viewer.add_image(rsize)
+viewer.add_image(ridges)
+viewer.add_image(mask)
+viewer.add_labels(markers)
+viewer.add_labels(labels)
+viewer.add_image(line)
+viewer.grid.enabled = True
+
+#%% Tests 
+
