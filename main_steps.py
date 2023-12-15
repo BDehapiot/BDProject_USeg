a73331e7495c9dc5bbf4bd6ba1ccac49b2c86c2e
bdehapiot <b.dehapiot@gmail.com>
2023-12-15 13:57:36 +0100

    update

--- main_steps.py
+++ main_steps.py
@@ -19,6 +19,12 @@ from skimage.morphology import binary_dilation, remove_small_objects, square
 from skel import labconn
 from nan import nanreplace
 
+#%% Comments ------------------------------------------------------------------
+
+'''
+- stop using cell_info and bound_info as dataframe
+'''
+
 #%% Execute -------------------------------------------------------------------
 
 # Paths
@@ -40,7 +46,7 @@ raw = io.imread(raw_path)
 # Parameters
 binning = 2
 ridge_size = 4
-thresh_coeff = 0.5
+thresh_coeff = 0.4
 small_cell_cutoff = 5
 large_cell_cutoff = 10
 remove_border_cells = False
@@ -83,8 +89,8 @@ def get_wat(
         # Filter cells (according to area)
         props = regionprops(markers)
         cell_info = pd.DataFrame(
-            ([(frame, i.label, i.area, 0) for i in props]),
-            columns = ['frame', 'idx', 'area', 'valid']
+            ([(frame, prop.label, prop.area, 0) for prop in props]),
+            columns = ['frame', 'label', 'area', 'valid']
             )
         median = np.median(cell_info['area'])
         for i in range(len(cell_info)):
@@ -92,7 +98,7 @@ def get_wat(
             # Detect & remove small cells
             if cell_info.loc[i,'area'] < median / small_cell_cutoff: 
                 cell_info.loc[i,'valid'] = 1
-                markers[markers==cell_info['idx'][i]] = 0  
+                markers[markers==cell_info['label'][i]] = 0  
             
             # Detect large cells
             if cell_info.loc[i,'area'] > median * large_cell_cutoff: 
@@ -107,7 +113,7 @@ def get_wat(
             ) 
 
         # Remove large cells
-        for idx in cell_info.loc[cell_info['valid'] == 2, 'idx']:
+        for idx in cell_info.loc[cell_info['valid'] == 2, 'label']:
             labels[labels==idx] = 0
             
         # Remove border cells
@@ -117,8 +123,8 @@ def get_wat(
         # Update cell info
         props = regionprops(labels)
         cell_info = pd.DataFrame(
-            ([(frame, i.label, i.area) for i in props]),
-            columns = ['frame', 'idx', 'area']
+            ([(frame, prop.label, prop.area) for prop in props]),
+            columns = ['frame', 'label', 'area']
             )
 
         # Get watershed wat
@@ -137,9 +143,9 @@ def get_wat(
         
         # Label bounds
         bound_labels = label(bounds, connectivity=2).astype('float')
-        bound_labels[endpoints == 1] = 0   
         bound_labels = expand_labels(bound_labels, distance=1.5)
-        
+        bound_labels[wat == 0] = 0
+               
         bound_labels = bound_labels.astype('int')
         small_bounds = wat ^ (bound_labels > 0) ^ vertices
         small_bound_labels = label(small_bounds, connectivity=2)
@@ -147,17 +153,24 @@ def get_wat(
         small_bound_labels[small_bound_labels == np.max(bound_labels)] = 0
         bound_labels = bound_labels + small_bound_labels
 
-        # Get bound intensities
+        # Get bound info
         props = regionprops(
             bound_labels, 
             intensity_image=(gaussian(rsize, ridge_size))
             )
 
-        bound_info = pd.DataFrame(([
-            (frame, prop.label, prop.intensity_mean) 
-            for prop in props]),
-            columns = ['frame', 'idx', 'int'],
-            )
+        # bound_info = pd.DataFrame(([
+        #     (frame, prop.label, prop.coords, prop.intensity_mean) 
+        #     for prop in props]),
+        #     columns = ["frame", "label", "coords", "intensity"],
+        #     )
+        
+        bound_info = [(
+            frame, prop.label, prop.intensity_mean, 
+            prop.coords
+            ) 
+            for prop in props
+            ]
         
         return (
             rsize, ridges, mask, markers, labels, wat, vertices, bound_labels,
@@ -207,12 +220,14 @@ def get_wat(
             [data[6] for data in outputs], axis=0).squeeze(),
         'bound_labels': np.stack(
             [data[7] for data in outputs], axis=0).squeeze(),
-        'cell_info' : pd.concat(
-            [(data[8]) for i, data in enumerate(outputs)]),
-        'bound_info' : pd.concat(
-            [(data[9]) for i, data in enumerate(outputs)]),        
+        # 'cell_info' : pd.concat(
+        #     [(data[8]) for i, data in enumerate(outputs)]),
+        # 'bound_info' : pd.concat(
+        #     [(data[9]) for i, data in enumerate(outputs)]),      
+        # 'bound_info' : [(data[9]) for i, data in enumerate(outputs)],  
+        'bound_info' : [item for data in outputs for item in data[9]],
         }
-    
+
     return data
 
 # -----------------------------------------------------------------------------
@@ -243,15 +258,54 @@ print(f" {(t1-t0):<5.2f}s")
 
 #%% Experiment ----------------------------------------------------------------
 
-import matplotlib.pyplot as plt
 
-# -----------------------------------------------------------------------------
 
-bound_info = data["bound_info"]
+# Get temp_mask
+temp_mask = labels > 0
+temp_mask = binary_dilation(
+    temp_mask, footprint=disk(ridge_size)
+    )
 
-# -----------------------------------------------------------------------------
+# Get bg_blur (with a nan ignoring Gaussian blur)
+temp1 = rsize.astype('float')
+temp1[temp_mask == 0] = np.nan
 
-plt.hist(bound_info['int'], bins=100, edgecolor='black')
+temp2 = temp1.copy()
+temp2[np.isnan(temp2)] = 0
+temp2_blur = gaussian(temp2, sigma=ridge_size*10)
+
+temp3 = 0 * temp1.copy() + 1
+temp3[np.isnan(temp3)] = 0
+temp3_blur = gaussian(temp3, sigma=ridge_size*10)
+
+np.seterr(divide='ignore', invalid='ignore')
+bg_blur = temp2_blur / temp3_blur
+
+# Get rsize_blur (divided by bg_blur)
+bound_norm = gaussian(rsize, sigma=ridge_size//2)
+bound_norm = bound_norm / bg_blur *1.5
+
+# from sklearn.mixture import GaussianMixture
+
+# bound_info = data["bound_info"]
+# bound_labels = data["bound_labels"].astype(float)
+# intensities = np.stack([data[2] for data in bound_info]).reshape(-1, 1)
+# frames = [data[0] for data in bound_info]
+# coords = [data[3] for data in bound_info]
+
+# gmm = GaussianMixture(n_components=2, random_state=0)
+# gmm.fit(intensities)
+# probabilities = gmm.predict_proba(intensities)[:, 0]
+
+# for i, (frame, coord) in enumerate(zip(frames, coords)):
+#     coord = (coord[:, 0], coord[:, 1])
+#     bound_labels[coord] = intensities[i]
+
+# io.imsave(
+#     Path("data", "temp", raw_path.stem + "_bound_probs.tif"),
+#     bound_labels.astype("float32"), check_contrast=False,
+#     )
+
 
 #%% Save ----------------------------------------------------------------------
   
